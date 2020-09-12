f8e548a8c635116b62be7c9f90d93407d54ee283
h7x4 <h7x4abk3g@protonmail.com>
2020-09-13 01:36:59 +0200

    Add reference for future https server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import https from 'https';
 
 import apiRouter from './routes/api';
 import mongoExpressRouter from './routes/mongoexpress';
@@ -14,7 +15,13 @@ app.use('/mongoexpress', mongoExpressRouter)
 const HOSTNAME = process.env.NODE_HOSTNAME;
 const PORT = process.env.NODE_PORT || 8080;
 
-app.listen({
-  port: PORT, 
-  callback: () => console.log(`Server listening at ${HOSTNAME}:${PORT}`)
-});
+app.listen(
+  PORT, 
+  () => console.log(`Server listening at ${HOSTNAME}:${PORT}`)
+);
+
+// https.createServer(
+//   {
+//     cert: '',
+//     key: ''
+//   }, app).listen(5000);
