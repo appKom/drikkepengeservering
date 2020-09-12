0919709c5f9ab8d761923b68c8f1bff2aba8b48f
h7x4 <h7x4abk3g@protonmail.com>
2020-09-13 01:36:01 +0200

    Add proper proxy library

--- src/routes/mongoexpress.ts
+++ src/routes/mongoexpress.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
+import proxy from 'express-http-proxy';
+
 const mongoExpressRouter = Router();
 
-mongoExpressRouter.get('', (req, res) => {
-  res.redirect('http://localhost:8081');
-});
+mongoExpressRouter.use(proxy('http://localhost:8081'));
 
 export default mongoExpressRouter;
