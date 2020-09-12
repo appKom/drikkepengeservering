1dadfb628f7b00639ccb3c6e66f9e44ead15b6d3
h7x4 <h7x4abk3g@protonmail.com>
2020-09-13 01:42:07 +0200

    Add some api functionality

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,10 +1,31 @@
 import { Router, json } from 'express';
+import user from '../models/user';
 const apiRouter = Router();
 
 apiRouter.use(json());
 
+
 apiRouter.get('/balance', (req, res) => {
-  res.send('');
+  res.send('Got balance')
+});
+
+apiRouter.post('/user', (req, res) => {
+  try {
+
+    const theUser = new user({
+      userId: req.body.userId,
+      rfid: req.body.rfid,
+      coins: 0,
+      createAt: Date.now()
+    });
+
+    theUser.save();
+    
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500)
+    res.send('Something went wrong');
+  }
 });
 
 
