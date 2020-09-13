49b44b4ae4924ad86d09b012cbf4f0016359f01c
h7x4 <h7x4abk3g@protonmail.com>
2020-09-13 17:39:53 +0200

    Add database config to main file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import express from 'express';
 import https from 'https';
 
+import mongoose from 'mongoose';
+
 import apiRouter from './routes/api';
 import mongoExpressRouter from './routes/mongoexpress';
 
 const app = express();
 
 // app.use(auth);
+
+export const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
+
+const MONGO_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+
+mongoose.connect(
+  MONGO_URL,
+  {useNewUrlParser: true},
+  () => console.log(`Connected to database ${MONGO_DB} at ${MONGO_HOSTNAME}:${MONGO_PORT}`)
+);
+
 
 app.use('/api/v1', apiRouter);
 app.use('/mongoexpress', mongoExpressRouter)
