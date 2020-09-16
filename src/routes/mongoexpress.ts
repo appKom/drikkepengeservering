7f479c614ebc1ff5f55b090a0ec23dfd4a8bc657
h7x4 <h7x4abk3g@protonmail.com>
2020-09-16 23:31:43 +0200

    Remove mongoexpress reverse proxy file

--- src/routes/mongoexpress.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import proxy from 'express-http-proxy';
-
-const mongoExpressRouter = Router();
-
-mongoExpressRouter.use(proxy('http://localhost:8081'));
-
-export default mongoExpressRouter;
