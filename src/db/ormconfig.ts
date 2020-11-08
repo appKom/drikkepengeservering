3b40a33f4ce9737b2a79118fb9e7ee6ccceaa4ac
h7x4 <h7x4abk3g@protonmail.com>
2020-11-08 15:09:11 +0100

    Centralize ormconfig

---
+++ src/db/ormconfig.ts
@@ -0,0 +1,34 @@
+import { ConnectionOptions } from "typeorm";
+
+const {
+  PG_USER,
+  PG_PASSWORD,
+  PG_DB
+} = process.env
+
+const ormConfig: ConnectionOptions = {
+  host: 'postgres',
+  type: 'postgres',
+  database: PG_DB || '',
+  username: PG_USER || '',
+  password: PG_PASSWORD || '',
+  synchronize: true,
+  logging: false,
+  migrationsTableName: 'migrations',
+  entities: [
+    'src/db/entity/**/*.ts'
+  ],
+  migrations: [
+    'src/db/migration/**/*.ts'
+  ],
+  subscribers: [
+    'src/db/subscriber/**/*.ts'
+  ],
+  cli: {
+    entitiesDir: 'src/db/entity',
+    migrationsDir: 'src/db/migration',
+    subscribersDir: 'src/db/subscriber'
+  }
+};
+
+export default ormConfig;
--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,12 @@ import https from 'https';
 import apiRouter from './routes/api';
 import pgAdminRouter from './routes/pgAdmin';
 
-export const {
-  PG_USER,
-  PG_PASSWORD,
-  PG_DB
-} = process.env;
-
-createConnection({
-  host: 'postgres',
-  type: 'postgres',
-  database: PG_DB,
-  username: PG_USER,
-  password: PG_PASSWORD,
-}).then(async (connection: Connection) => {  
+import ormConfig from './db/ormconfig'
+createConnection(ormConfig)
+  .then(async (connection: Connection) => {
   console.log('Successfully connected to database!');
 
-  const app = express();  
+  const app = express();
   // app.use(auth);
 
   app.use('/api/v1', apiRouter);
@@ -28,16 +18,16 @@ createConnection({
 
   const HOSTNAME = process.env.NODE_HOSTNAME;
   const PORT = process.env.NODE_PORT || 8080;
-  
+
   app.listen(
-    PORT, 
+    PORT,
     () => console.log(`Server listening at ${HOSTNAME}:${PORT}`)
   );
-  
+
   // https.createServer(
   //   {
   //     cert: '',
   //     key: ''
   //   }, app).listen(5000);
-  
+
 }).catch((error: Error) => console.log(error));
